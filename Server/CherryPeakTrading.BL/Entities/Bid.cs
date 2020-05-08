7f31125379adada1973bd7a4dd6cb27f27d49135
Olesia-Timofeeva-EPAM <olesia_timofeeva@epam.com>
2020-05-08 18:34:40 +0400

    Create solution and back-end projects

---
+++ Server/CherryPeakTrading.BL/Entities/Bid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CherryPeakTrading.BL.Entities
+{
+	public class Bid
+	{
+		public long Id { get; set; }
+
+		public int UserId { get; set; }
+
+		public int LotIt { get; set; }
+
+		public DateTime TimeStamp { get; set; }
+
+		public double BidAmount { get; set; } 
+	}
+}
---
+++ Server/CherryPeakTrading.Data/Entities/Lot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CherryPeakTrading.Data.Entities
+{
+	public class Lot 
+	{
+		public long Id { get; set; }
+
+		public string Title { get; set; }
+
+		public string Description { get; set; }
+
+		public LotPicture Picture { get; set; }
+
+		public decimal MinPrice { get; set; }
+
+		public int CreatorId { get; set; }
+
+		public int RegionID { get; set; }
+
+		public int TimeToLeave { get; set; }
+
+		public int TimeStep { get; set; }
+
+		public int StatusId { get; set; }
+
+		public DateTime StartTime { get; set; }
+
+		public DateTime EndTime { get; set; }
+
+		public List<Bid> Bids { get; set; }
+
+	}
+}
