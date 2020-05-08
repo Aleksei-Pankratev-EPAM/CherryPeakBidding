458a170588a708bd6fd52b16e9e8f821dcc66c5a
Olesia-Timofeeva-EPAM <olesia_timofeeva@epam.com>
2020-05-08 18:34:40 +0400

    Create solution and back-end projects

---
+++ Server/CherryPeakTrading.BL/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CherryPeakTrading.BL.Entities
+{
+	public class User
+	{
+		public long Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Email { get; set; }
+
+		public int RegionId { get; set; }
+
+		public int PersonalAccountId { get; set; }
+	}
+}
---
+++ Server/CherryPeakTrading.Data/Entities/LotPicture.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CherryPeakTrading.Data.Entities
+{
+	public class LotPicture
+	{
+		public long Id { get; set; }
+	}
+}
