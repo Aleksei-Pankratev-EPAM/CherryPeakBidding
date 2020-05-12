50b210aaf7f0961d53d6debb08c8f884fad4d1ea
Sergei Antonov <Sergei_Antonov2@epam.com>
2020-05-12 14:10:30 +0400

    Adapt the logo and sidebar sizes. Set min-width to the sidebar. Fix the width of the page content

--- Client/src/components/Logo.js
+++ Client/src/components/Logo.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import '../css/Logo.css';
+
 function Logo() {
     return (
         <div className="logo d-flex justify-content-center">
             <Link to="/">
-                <img className="text-center" src="/logo.png" alt="Cherry Peak Bidding" width="180" height="170" />
+                <img className="text-center" src="/logo.png" alt="Cherry Peak Bidding" />
             </Link>
         </div>
     );
