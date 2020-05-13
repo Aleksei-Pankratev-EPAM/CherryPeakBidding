0674cf6838e82805c29a9200ef69c6c6d51e80c6
Aleksei Pankratev <Aleksei_Pankratev@epam.com>
2020-05-13 14:35:19 +0400

    Provide string.Empty as a default value from string properties

--- Server/CherryPeakTrading.BL/Entities/User.cs
+++ Server/CherryPeakTrading.BL/Entities/User.cs
@@ -4,9 +4,9 @@
     {
         public long Id { get; set; }
 
-        public string Name { get; set; } = "";
+        public string Name { get; set; } = string.Empty;
 
-        public string Email { get; set; } = "";
+        public string Email { get; set; } = string.Empty;
 
         public int RegionId { get; set; }
 
