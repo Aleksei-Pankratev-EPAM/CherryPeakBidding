eb2c9592d9e66d9fe3055616585d2bf5099b8125
Aleksei Pankratev <Aleksei_Pankratev@epam.com>
2020-05-14 12:36:39 +0400

    Refactor logging infrastructure to make it reusable by service workers

--- Server/CherryPeakTrading.eBiddingAPI/Program.cs
+++ Server/CherryPeakTrading.eBiddingAPI/Program.cs
@@ -1,11 +1,8 @@
+using CherryPeakTrading.DI.Logging;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
-using Serilog.Formatting.Elasticsearch;
-using Serilog.Formatting.Json;
-using Serilog.Sinks.Elasticsearch;
-using Serilog.Sinks.RollingFile;
 using System;
 using System.IO;
 
@@ -21,7 +18,7 @@ namespace CherryPeakTrading.eBidding
 
         public static void Main(string[] args)
         {
-            CreateLogger();
+            StructuredLoggingExtensions.CreateGlobalLogger(Configuration.GetConnectionString("ElasticSearch"));
 
             try
             {
@@ -45,28 +42,5 @@ namespace CherryPeakTrading.eBidding
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-
-        private static void CreateLogger()
-        {
-            Log.Logger = new LoggerConfiguration()
-                  .Enrich.WithMachineName()
-                  .Enrich.WithEnvironmentUserName()
-                  .Enrich.FromLogContext()
-                  .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(Configuration.GetConnectionString("ElasticSearch")))
-                  {
-                      // Write a default template to Elasticsearch
-                      AutoRegisterTemplate = true,
-                      AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6,
-                      // Serialize any exception into an exception object
-                      CustomFormatter = new ExceptionAsObjectJsonFormatter(renderMessage: true),
-                      // Handle issues with Elasticsearch
-                      FailureCallback = e => Console.WriteLine("Unable to submit event " + e.MessageTemplate),
-                      EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog |
-                                       EmitEventFailureHandling.WriteToFailureSink |
-                                       EmitEventFailureHandling.RaiseCallback,
-                      FailureSink = new RollingFileSink("./failureSink.txt", new JsonFormatter(), null, null)
-                  })
-                  .CreateLogger();
-        }
     }
 }
